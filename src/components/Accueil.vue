e3b6d66993e2a21832aecf23021722c9a5f5bed6
Big-pun <150970603+Big-pun@users.noreply.github.com>
2024-11-21 18:56:25 -0500

    first commit

---
+++ src/components/Accueil.vue
@@ -0,0 +1,17 @@
+<template>
+  <h1>Accueil</h1>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      
+    };
+  },
+};
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/Produits.vue
@@ -0,0 +1,17 @@
+<template>
+  <h1>Liste des produits</h1>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      
+    };
+  },
+};
+</script>
+
+<style scoped>
+
+</style>
